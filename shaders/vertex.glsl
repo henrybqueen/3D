4ed00bf002a29097db8507bf6e9e8b7f0585b032
unknown <henrybqueen@gmail.com>
2024-03-04 14:58:25 -0800

    fix vertex shader

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -6,11 +6,14 @@ layout (location = 1) in vec3 normal;
 out vec3 fragPos;
 out vec3 fragNormal;
 
+uniform mat4 view;
+uniform mat4 proj;
+
 
 void main() {
 
     fragPos = pos;
     fragNormal = normal;
 
-    gl_Position = vec4(pos, 1.0);
+    gl_Position = proj * view * vec4(pos, 1.0);
 }
